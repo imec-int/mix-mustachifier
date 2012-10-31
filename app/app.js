bde387b6d3333c6c42978b55d2a0df22426484a9
Matthias De Geyter <matthias.degeyter@ibbt.be>
2012-10-31 14:30:06 +0100

    foto's posten via twitpic en wat queries (user, tweets) toegevoegd

--- app/app.js
+++ app/app.js
@@ -3,14 +3,16 @@ var http 		= require('http-get');
 var Step 		= require('step');
 var socketio	= require('socket.io');
 var async		= require('async');
-
+var OAuth = require('oauth').OAuth;
+var keys = require('./twitterkeys');
+var TwitPic = require('twitpic').TwitPic;
 
 /**
  * Webserver stuff:
  */
  var webserver = module.exports = express.createServer();
  webserver.configure(function(){
- 	webserver.set('views', __dirname + '/views');
+	webserver.set('views', __dirname + '/views');
 	webserver.set('view engine', 'jade');
 	webserver.use(express.bodyParser());
 	webserver.use(express.methodOverride());
@@ -75,6 +77,33 @@ io.sockets.on('connection', function (socket) {
 });
 
 
+// twitter initialisatie
+
+var tp = new TwitPic();
+tp.config(function (config) {
+  config.apiKey = keys.twitpickey;
+  config.consumerKey = keys.consumerKey;
+  config.consumerSecret = keys.consumerSecret;
+  config.oauthToken = keys.token;
+  config.oauthSecret = keys.secret;
+});
+
+// authentication for other twitter requests
+var tweeter = new OAuth(
+	"https://api.twitter.com/oauth/request_token",
+	"https://api.twitter.com/oauth/access_token",
+	keys.consumerKey,
+	keys.consumerSecret,
+	"1.0",
+	null,
+	"HMAC-SHA1"
+);
+
+
+
+
+
+
 /**
  * Webserver routes
  */
@@ -101,6 +130,91 @@ webserver.get('/wall', function (req, res){
 });
 
 
+
+// POST PICTURE to TWITTER
+
+// ?picname=bla&message=blo
+webserver.get('/tweetimage', function(req, res){
+	tp.uploadAndPost({path: __dirname + "/public/mustacheimages/" + req.query.picname, message: req.query.message}, function (data) {
+	  // console.log(data);
+	});
+	res.end();
+});
+
+
+// GET USERDETAILS + TWEETS
+
+// /userinfo?id=twitterhandle
+webserver.get('/userinfo', function(req, res){
+	var message = {};
+	var recenttweets = [];
+	// %23 doet url-encoding van de hashtag
+	tweeter.getProtectedResource('https://api.twitter.com/1.1/users/show.json?screen_name=%23'+req.query.id,
+		"GET", keys.token, keys.secret,
+		function(error, data, response){
+			if(error) {
+				console.log('Error: '+ JSON.stringify(error)+'\n');
+				res.json({err: error});
+			}
+			else {
+				data = JSON.parse(data);
+				message = {name: data.name,
+					profileimage: data.profile_image_url,
+					twitterhandle: data.screen_name,
+					description: data.description
+				};
+
+				tweeter.getProtectedResource('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+req.query.id,
+					"GET", keys.token, keys.secret,
+					function(error2, data2, response){
+						if(error2)
+							res.json(message);
+						else{
+							data2=JSON.parse(data2);
+							for(var i=0; i< data2.length; i++){
+								recenttweets[i] = {text: data2[i].text,
+									created_at: data2[i].created_at,
+									name: data2[i].user.name,
+									profileimage: data2[i].user.profile_image_url,
+									twitterhandle: data2[i].user.screen_name
+								}
+							}
+							message.tweets = recenttweets;
+							res.json(message);
+						}
+					}
+				);
+			}
+		}
+	);
+});
+
+// SEARCH TWEETS for # q
+
+// search tweets ?q=
+webserver.get('/search', function(req, res){
+	// console.log(req.query.q);
+	tweeter.getProtectedResource('https://api.twitter.com/1.1/search/tweets.json?q='+req.query.q,
+		"GET", keys.token, keys.secret,
+		function(error, data, response){
+			if(error){
+				res.json({err: error});
+			}
+			else{
+				var tweets = [];
+				data = JSON.parse(data);
+				for(var i=0; i<data.statuses.length; i++){
+					tweets[i] = { text: data.statuses[i].text,
+						created_at: data.statuses[i].created_at,
+						profileimage: data.statuses[i].user.profile_image_url,
+						name: data.statuses[i].user.name,
+						twitterhandle: data.statuses[i].user.screen_name
+					};
+				}
+				res.json(tweets);
+			}
+	});
+});
 
 
 
