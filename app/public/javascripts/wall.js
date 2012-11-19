b50e55c1c8185890148dc8573e543af209e287a1
Sam Decrock <sam.decrock@gmail.com>
2012-11-19 17:14:14 +0100

    live tweets now fade in

--- app/public/javascripts/wall.js
+++ app/public/javascripts/wall.js
@@ -1,55 +1,16 @@
 App = {
 	picturesInCache:{},
 	offsetLeft: 0,
-
-	randomArticles:[
-		{
-			title: "iMinds/Bell Labs Open Day joint plenary: Wim De Waele, Markus Hofmann, Andrew Keen",
-			time: "9:00 - 10:30",
-			image: "images/wimdewaele.jpg",
-			content: "Wim De Waele is Chief Executive Officer of iMinds, headquartered in Ghent, Belgium. iMinds specializes in interdisciplinary research and development in the software sector, working with companies and other partners on the newest technologies in domains such as networking, media, security and healthcare.<br />Besides his general management duties, he  also leads the market strategy and incubation efforts of iMinds with strong focus on the development of new start-up companies and business acceleration of technology concepts.",
-			width: 215
-
-		},
-		{
-			title: "MiXing things up: should content be a freebie?",
-			time: "11:00 - 12:30",
-			image: "images/mix.png",
-			content: 'The path to Tomorrow\'s Media is studded with questions, doubt, and also fear of the unknown. Each issue evokes a multitude of new questions and uncertainties. Who knows what media consumption will look like in the future? Who knows whether content will maintain its value? And especially; how will it be valorized? What does "online freedom" mean? We kindly invite you to listen to two outspoken opinions on that future. Two passionate people delivering two diverging views. No questions, but answers!',
-			width: 215
-		},
-		{
-			title: "Jim McKelvey: Square",
-			time: "14:00 - 14:30",
-			image: "images/jimmckelvey2.jpg",
-			content: "This year Jim McKelvey will be speaking at iMinds. Jim McKelvey is an American computer science engineer and businessperson widely known as the co-founder of Square, a mobile payments company. Square is an electronic payment service, provided by Square Inc. Square allows users in the United States to accept credit cards through their mobile phones, either by swiping the card on the Square device or by manually entering the details on the phone.",
-			width: 215
-		}
-	],
 	timeouthandle:'',
-	timeouthandle2:'',
+	timeoutAfterpublishHandle:'',
 	counter: 0,
 	slidenumber: 1,
 	timeoutvalue: 30000,
 	showingtwitter: false,
 	start: function (){
-		console.log("hello world");
-
-
-
-		App.articleCollection = new App.ArticleCollection();
+		App.moustacheCollection = new App.MoustacheCollection();
 		App.tweetsCollection = new App.TweetsCollection();
 		App.articlelistView = new App.ArticlelistView();
-		// TOCH MAAR NIET; beter voor carrousel
-		// check for checkbox changes
-		// var twitterbox = $('input:checkbox[name=twitter]');
-		// twitterbox.click(function() {
-		// 	var checked = false;
-		// 	if(twitterbox.attr('checked'))
-		// 		checked = true;
-		// 	$.post("/rest/showtwitterfeed", {checked: checked});
-
-		// });
 
 		// socket.io initialiseren
 		App.socket = io.connect(window.location.hostname);
@@ -72,12 +33,12 @@ App = {
 				tweets: data.tweets
 			});
 			if(App.timeouthandle) clearTimeout(App.timeouthandle);
-			if(App.timeouthandle2) clearTimeout(App.timeouthandle2);
+			if(App.timeoutAfterpublishHandle) clearTimeout(App.timeoutAfterpublishHandle);
 			App.counter = 0;
 			App.slidenumber = 1;
-			App.timeouthandle2= setTimeout(App.rotateStuff, App.timeoutvalue);
+			App.timeoutAfterpublishHandle= setTimeout(App.rotateStuff, App.timeoutvalue);
 
-			App.articleCollection.add(articleModel);
+			App.moustacheCollection.add(articleModel);
 		});
 
 		App.socket.on('wall.showtweets', function (data) {
@@ -90,18 +51,29 @@ App = {
 		});
 
 		App.socket.on('wall.newtweet', function (data) {
-			var thetemplate= $("#singletweetpl");
-			var html = thetemplate.tmpl(data);
-			html.prependTo($(".bigtweets").last());
-		});
-		// make urls work -- NO
-		// $("a[title='moust']").click(function(){
-		// 	if(App.articleCollection.length>0){
-		// 		var lastphoto = App.articleCollection.last();
-		// 		App.articlelistView.renterPicture(lastphoto);
-		// 	}
-		// });
 
+			function addTweet(data){
+				var thetemplate= $("#singletweetpl");
+				var html = thetemplate.tmpl(data);
+
+				$(html).hide();
+				html.prependTo($(".bigtweets").last());
+				$(html).fadeIn();
+
+			}
+
+			if(data.profileimage){
+				var preloadImage = new Image();
+				preloadImage.onload = function(){
+					addTweet(data);
+				}
+				preloadImage.src = data.profileimage;
+			}else{
+				addTweet(data);
+			}
+
+
+		});
 	},
 
 	rotateStuff: function(){
@@ -111,8 +83,8 @@ App = {
 		// laatste foto's opnieuw tonen
 		if(moustachebox.attr('checked'))
 		{
-			if(App.articleCollection.length>1 && App.counter<App.articleCollection.length){
-				App.articlelistView.renterPicture(App.articleCollection.at(App.counter));
+			if(App.moustacheCollection.length>1 && App.counter<App.moustacheCollection.length){
+				App.articlelistView.renterMoustache(App.moustacheCollection.at(App.counter));
 				App.showingtwitter = false;
 				App.counter+=1;
 				App.timeouthandle = setTimeout(App.rotateStuff, App.timeoutvalue);
@@ -167,15 +139,7 @@ App = {
 		}
 	},
 
-	insertRandomArticle: function (){
-		var randomIndex = Math.floor(Math.random()*App.randomArticles.length);
-
-		var articleModel = new App.ArticleModel(App.randomArticles[randomIndex]);
-		App.articleCollection.add(articleModel);
-	},
-
 	doSlide: function(){
-
 		var firstarticle = $('.article').first();
 		firstarticle.animate({width: '0'}, 1000, function(){firstarticle.remove();});
 	}
@@ -187,11 +151,11 @@ App.ArticlelistView = Backbone.View.extend({
 	el: "#articles",
 
 	initialize: function(){
-		App.articleCollection.bind("add", this.renterPicture, this);
+		App.moustacheCollection.bind("add", this.renterMoustache, this);
 		App.tweetsCollection.bind("add", this.renderTweets, this);
 	},
 
-	renterPicture: function(model){
+	renterMoustache: function(model){
 		var self = this;
 
 		// image preloaden:
@@ -276,7 +240,7 @@ App.TweetsModel = Backbone.Model.extend({
 });
 
 // Backbone Collection
-App.ArticleCollection = Backbone.Collection.extend({
+App.MoustacheCollection = Backbone.Collection.extend({
 	model: App.ArticleModel
 });
 
