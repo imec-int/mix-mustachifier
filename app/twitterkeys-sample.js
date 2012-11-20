102b6477941b5de314b1660a4a85657f4abf9060
Sam Decrock <sam.decrock@gmail.com>
2012-11-20 16:45:55 +0100

    twitter keys sample

---
+++ app/twitterkeys-sample.js
@@ -0,0 +1,7 @@
+// Enter your Twitter keys here
+
+// These keys will NOT work ;-)
+exports.consumerKey = "AaAaAaAaAaAaAaAaAaAaA";
+exports.consumerSecret = "AaAaAaAaAaAaAaAaAaAaAAaAaAaAaAaAaAaAaAaAaA";
+exports.token = "000000000-AaAaAaAaAaAaAaAaAaAaAAaAaAaAaAaAaAaAaAaA";
+exports.secret = "AaAaAaAaAaAaAaAaAaAaAAaAaAaAaAaAaAaAaAaA";
